551cf10a0546c200cfe9d7018dbb3bdd91c49b2b
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-29 13:40:40 +0000

    Add mailer

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+ def user
+   user = User.new(name: "テスト太郎", login: "お問い合わせメッセージ")
+
+   UserMailer.send_mail(user)
+ end
 
 end
